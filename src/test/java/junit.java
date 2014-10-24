f89e8b78a1d7f71c4c8463109f44f5a7834fcde4
sleepbear669 <sleepbear669@gmail.com>
2014-10-24 14:49:34 +0900

    Initial commit

---
+++ src/test/java/junit.java
@@ -0,0 +1,14 @@
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+/**
+ * Created by sleepbear on 2014-10-24.
+ */
+public class junit{
+    @Test
+    public void junit() throws Exception {
+   
+    }
+}
